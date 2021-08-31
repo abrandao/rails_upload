55cde9686ee36aed26f67b08967eb252dccf4add
brandao <abrandao>
2021-08-31 11:55:56 -0300

    upload and save working

--- ror/app/controllers/purchases_controller.rb
+++ ror/app/controllers/purchases_controller.rb
@@ -1,5 +1,37 @@
+require 'csv'
+
 class PurchasesController < ApplicationController
+  
   def index
     @purchases = Purchase.all
+  end
+
+  def upload
+    @uploaded_file = params[:file]
+    
+    CSV.foreach(@uploaded_file, headers: true, col_sep: "\t") do |row|
+      purchase = row.to_h
+
+      Purchaser.create(
+        name: purchase['purchaser name'],
+      )
+
+      Merchant.create(
+        name: purchase['merchant name'],
+        address: purchase['merchant address']
+      )
+
+      Item.create(
+        description: purchase['item description'],
+        price: purchase['item price']
+      )
+
+      Purchase.create(
+        count: purchase['purchase count'],
+        purchaser_id: Purchaser.last.id,
+        item_id: Item.last.id,
+        merchant_id: Merchant.last.id
+      )
+    end
   end
 end
--- ror/config/routes.rb
+++ ror/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root 'purchases#index'
+
+  resources :purchases, only: [:index]
+  post 'uploads', to: 'purchases#upload', as: 'uploads'
 end
